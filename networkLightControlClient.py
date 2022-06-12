8d0826fa652ade46449071a744b9494fd0eee9f3
Chris Bennett <christopher@mlbennett.com>
2022-06-13 00:44:42 +0100

    Initial commit

---
+++ networkLightControlClient.py
@@ -0,0 +1,155 @@
+import socket
+import math
+import time
+import threading
+
+
+class networkControlledStrip:
+    def __init__(self, ip:str, port:int=1337):
+        # connect to the strip and init arrays
+
+        # networking vars
+        self.__DISCONNECT_MESSAGE = b"DISCONNECT"
+        self.__DESIRED_BUFFER_SIZE = 2048
+        self.__KEEPALIVE_MSG = b"KEEPALIVE"
+
+        # init socket
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.__s.connect((ip, port))
+        except:
+            print(f"Failed to connect to socket at {ip}:{port}")
+            return -1
+        
+        # listen for LED count message from the server
+        knowLEDCount = False
+        while not knowLEDCount:
+            LEDCountMessage = self.__s.recv(128)
+            if len(LEDCountMessage) != 0:
+                LEDCountInt = int.from_bytes(LEDCountMessage, byteorder="big")
+                knowLEDCount = True
+                self.LED_COUNT = LEDCountInt
+        
+        # send desired buffer size to server
+        self.__s.send(self.__DESIRED_BUFFER_SIZE.to_bytes(2, byteorder="big"))
+
+        # listen for server confirmation of buffer size
+        bufferSizeConfirmed = False
+        while not bufferSizeConfirmed:
+            bufferSizeConfirmationMessage = self.__s.recv(128)
+            if len(bufferSizeConfirmationMessage) != 0:
+                if int.from_bytes(bufferSizeConfirmationMessage, byteorder="big") == self.__DESIRED_BUFFER_SIZE:
+                    bufferSizeConfirmed = True
+                else:
+                    print("Server failed to confirm buffer size")
+                    return -1
+        
+        # initialise write mask as array of length LEDCount full of Falses
+        self.__write_mask = [False] * self.LED_COUNT
+
+        # initialise colour data as array of length LEDCount full of arrays of length 4 full of 0s
+        self.__colour_data = [0] * self.LED_COUNT
+        for i in range(0, self.LED_COUNT):
+            self.__colour_data[i] = [0,0,0,0]
+
+        # start keepAlive thread
+        self.__connectionTerminating = False
+        self.__keepAlive = threading.Thread(target=self.keepAlive)
+        self.__keepAlive.start()
+
+        # wait a moment to avoid messages getting overlapped because sockets are DUMB
+        time.sleep(0.5)
+
+    def keepAlive(self):
+        # send a keepAlive message to the server every 30(ish) seconds
+        # this method is run in a seperate thread
+        while True:
+            for i in range(0, 60):
+                # check if thread has been told to stop every 0.5s so overall connection shutdown can be faster without a race condition
+                if self.__connectionTerminating:
+                    break
+                time.sleep(0.5)
+            self.__s.send(self.__KEEPALIVE_MSG)
+
+    def disconnect(self):
+        # disconnect from the server
+
+        # shut down keepAlive thread
+
+
+        self.__s.send(self.__DISCONNECT_MESSAGE)
+    
+    def setAllPixels(self, w:int, r:int, g:int, b:int):
+        # set every pixel value to the set colour
+
+        self.__write_mask = [True] * self.LED_COUNT
+
+        for i in range(0, self.LED_COUNT):
+            self.__colour_data[i] = [w, r, g, b]
+    
+    def setPixelColour(self, LEDID:int, w:int, r:int, g:int, b:int):
+        # alter internal arrays to reflect changed colour
+
+        # do a bunch of checks
+        if LEDID < 0 or LEDID > self.LED_COUNT:
+            print(f"Invalid LED ID of {LEDID}")
+            return -1
+        elif w < 0 or w > 255:
+            print(f"Invalid white value of {w}")
+            return -1
+        elif r < 0 or r > 255:
+            print(f"Invalid red value of {r}")
+            return -1
+        elif g < 0 or g > 255:
+            print(f"Invalid green value of {g}")
+            return -1
+        elif b < 0 or b > 255:
+            print(f"Invalid blue value of {b}")
+            return -1
+
+        self.__write_mask[LEDID] = True
+
+        self.__colour_data[LEDID] = [w, r, g, b]
+
+        return 0
+
+    def send(self):
+        # send accumulated changes over the network as a command
+        
+        messageToSend = bytearray(24)
+
+        binaryStringToConvertToWriteMask = ""
+        for i in range(0, self.LED_COUNT):
+            if self.__write_mask[i]:
+                binaryStringToConvertToWriteMask += "1"
+            else:
+                binaryStringToConvertToWriteMask += "0"
+        writeMaskInt = int(binaryStringToConvertToWriteMask, 2)
+        writeMaskBytes = writeMaskInt.to_bytes(math.ceil(self.LED_COUNT/8), byteorder="big")
+
+        messageToSend += writeMaskBytes
+
+        colourDataByteArray = bytearray(0) # init empty to append to
+        for i in range(0, self.LED_COUNT):
+            if self.__write_mask[i]:
+                colourDataByteArray += self.__colour_data[i][0].to_bytes(1, byteorder="big")
+                colourDataByteArray += self.__colour_data[i][1].to_bytes(1, byteorder="big")
+                colourDataByteArray += self.__colour_data[i][2].to_bytes(1, byteorder="big")
+                colourDataByteArray += self.__colour_data[i][3].to_bytes(1, byteorder="big")
+        
+        messageToSend += colourDataByteArray
+
+        # now that we're done reading them, clear the data arrays for next time
+        self.__write_mask = [False] * self.LED_COUNT
+        self.__colour_data = [0] * self.LED_COUNT
+        for i in range(0, self.LED_COUNT):
+            self.__colour_data[i] = [0,0,0,0]
+
+        # send to socket
+        try:
+            self.__s.send(messageToSend)
+        except:
+            print("Failed to send message to socket!")
+            return -1
+        
+        return 0
---
+++ server.py
@@ -0,0 +1,147 @@
+import socket
+from rpi_ws281x import PixelStrip, Color, ws
+import threading
+import time
+import math
+
+# server vars
+PORT = 1337
+SERVER = "192.168.1.233"
+ADDR = (SERVER, PORT)
+DISCONNECT_MESSAGE = b"DISCONNECT"
+KEEPALIVE_MESSAGE = b"KEEPALIVE"
+DEFAULT_BUFFER_SIZE = 128
+
+# LED vars
+LED_COUNT = 30
+LED_PIN = 18
+LED_FREQ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+LED_STRIP = ws.SK6812W_STRIP
+
+# server init
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+server.bind(ADDR)
+
+# LED init
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+strip.begin()
+
+def decodeAndApplyCommand(command):
+    # command must be a bytes object
+
+    # check header integrity
+    if int(command[:24].hex(), 16):
+        return 1 # error code 1 (incorrect header)
+
+    # as this function does not handle headers, discard it
+    command = command[24:]
+
+    # the first ceil(LED_COUNT/8) bytes are writeMask bytes, so extract them
+    writeMaskByteCount = math.ceil(LED_COUNT/8)
+    writeMaskBytes = command[:writeMaskByteCount]
+    command = command[writeMaskByteCount:] # remove writeMask from command
+    writeMaskStr = bin(int(writeMaskBytes.hex(), 16)).lstrip("0b")
+
+    # find expected length of message
+    numOfExpectedColourDatapoints = writeMaskStr.count("1")
+
+    # check length of message
+    if len(command)/4 < numOfExpectedColourDatapoints:
+        return 3 # error code 3 (message is too short)
+    elif len(command)/4 > numOfExpectedColourDatapoints:
+        return 2 # error code 2 (message is too long)
+
+    # loop over the remaining bytes, extracting in groups of 4 and applying
+    colourDataReadPoint = 0
+    for i in range(0, LED_COUNT):
+        if int(writeMaskStr[i]):
+            #print(command[4*colourDataReadPoint:4*(colourDataReadPoint+1)].hex())
+            strip.setPixelColor(i, int(command[4*colourDataReadPoint:4*(colourDataReadPoint+1)].hex(), 16))
+            colourDataReadPoint += 1
+
+    # apply changes to strip
+    strip.show()
+
+    # return 0 for success I guess?
+    time.sleep(0.001)
+    return 0
+
+def handle_client(conn, addr): # to be run in a seperate thread for every connection
+    bufferSize = DEFAULT_BUFFER_SIZE
+
+    # send client number of LEDs connected
+    conn.send(LED_COUNT.to_bytes(2, byteorder="big"))
+    print(f"Informed client {addr} of LED count {LED_COUNT}")
+
+    # client's first message must define buffer size
+    while True:
+        bufferSizeExchange = conn.recv(bufferSize)
+        if len(bufferSizeExchange) != 0:
+            bufferSize = int.from_bytes(bufferSizeExchange, byteorder="big")
+            print(f"Client {addr} has set buffer size to {bufferSize}")
+
+            # confirm buffer size to client
+            conn.send(bufferSize.to_bytes(2, byteorder="big"))
+
+            break
+
+    # start timeout by taking current time
+    lastMessageTime = time.time()
+
+    # listen for client commands
+    while True:
+        skipExec = False
+        conn.settimeout(15)
+        try:
+            command = conn.recv(bufferSize)
+        except socket.timeout:
+            command = ""
+        except ConnectionResetError:
+            print(f"Client {addr} has disconnected ungracefully")
+            break
+
+        if len(command) != 0:
+            # reset timeout
+            lastMessageTime = time.time()
+
+            # test if the message is a disconnect message
+            if command.startswith(DISCONNECT_MESSAGE):
+                print(f"Client {addr} has disconnected gracefully")
+                break
+
+            # test if the message is a keepAlive
+            if command.startswith(KEEPALIVE_MESSAGE):
+                print(f"Recieved keepAlive from {addr}")
+                lastMessageTime = time.time()
+                skipExec = True
+
+            if not skipExec:
+                try:
+                    conn.send(decodeAndApplyCommand(command).to_bytes(1, byteorder="big"))
+                except:
+                    print(f"Failed to parse in connection to {addr}")
+                    conn.send((4).to_bytes(1, byteorder="big"))
+
+        else:
+            if time.time() - lastMessageTime >= 30:
+                print(f"Client {addr} has timed out")
+                break
+            time.sleep(0.001)
+
+
+def start():
+    server.listen(5)
+    while True:
+        conn, addr = server.accept()
+        # server.accept() is blocking, so spin off a thread to handle the new connection
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"{threading.activeCount() - 1} active connections")
+
+print("Server Starting")
+start()
