1723dcd885145663c55ba7b78ee3f67c510398d6
Chris Bennett <christopher@mlbennett.com>
2022-06-15 17:30:39 +0100

    screw this just make it simple

--- runOnServerStart.py
+++ runOnServerStart.py
@@ -7,23 +7,10 @@ import socket
 def startScript():
     strip = networkControlledStrip(socket.gethostname())
 
-    lightsArr = [0]*strip.LED_COUNT
-
     for i in range(strip.LED_COUNT):
-        lightsArr[i] = [0,0,0,0]
 
-    for i in range(strip.LED_COUNT*2):
-        
-        # decrement everything for fading trail
-        for j in lightsArr:
-            for k in j:
-                k = max(0, k-16)
-        
-        lightsArr[i][1] = 255
-        lightsArr[i][3] = 127
-
-        for j in lightsArr:
-            strip.setPixelColour(i%2, lightsArr[i][0], lightsArr[i][1], lightsArr[i][2], lightsArr[i][3])
-        
+        strip.setAllPixels(0, 0, 0, 0)
+        strip.setPixelColour(i, 0, 255, 0, 127)
         strip.send()
+
         time.sleep(0.03)
